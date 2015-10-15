4c4e9f8b3b0b5907fd494fb7071d983bf157c8f9
jin-yc10 <ccjinyc@gmail.com>
2015-10-15 16:30:47 +0800

    format route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,9 +30,9 @@ Route::group([
     'prefix' => 'Auth',
     'middleware' => 'request.log'
     ], function(){
-    Route::get('login', "Auth\AuthController@getLogin");
-    Route::post('login', "Auth\AuthController@postLogin");
-    Route::get('register', "Auth\AuthController@getRegister");
+    Route::get( 'login',    "Auth\AuthController@getLogin");
+    Route::post('login',    "Auth\AuthController@postLogin");
+    Route::get( 'register', "Auth\AuthController@getRegister");
     Route::post('register', "Auth\AuthController@postRegister");
-    Route::get('logout', "Auth\AuthController@getLogout");
+    Route::get( 'logout',   "Auth\AuthController@getLogout");
 });
